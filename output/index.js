8c7a6bb280db7f417716b5c0f184e7a15bfd1d58
Alain <yinxulai@hotmail.com>
2026-03-22 11:51:18 +0800

    chore(release): bump version to 0.7.1

--- output/index.js
+++ output/index.js
@@ -1,3 +1,21 @@
+/**
+ * 创建 Airx 的 Vite 插件。
+ *
+ * 插件会将 JSX 转换目标设置为 Airx 运行时：
+ * - jsxFactory: __airx__.createElement
+ * - jsxFragment: __airx__.Fragment
+ * - jsxInject: import * as __airx__ from 'airx'
+ *
+ * @returns Vite 插件对象。
+ *
+ * @example
+ * import { defineConfig } from 'vite'
+ * import airx from 'vite-plugin-airx'
+ *
+ * export default defineConfig({
+ *   plugins: [airx()]
+ * })
+ */
 export function VitePluginAirx() {
     return {
         name: 'airx',
--- source/index.ts
+++ source/index.ts
@@ -1,5 +1,23 @@
 import { UserConfig } from 'vite'
 
+/**
+ * 创建 Airx 的 Vite 插件。
+ *
+ * 插件会将 JSX 转换目标设置为 Airx 运行时：
+ * - jsxFactory: __airx__.createElement
+ * - jsxFragment: __airx__.Fragment
+ * - jsxInject: import * as __airx__ from 'airx'
+ *
+ * @returns Vite 插件对象。
+ *
+ * @example
+ * import { defineConfig } from 'vite'
+ * import airx from 'vite-plugin-airx'
+ *
+ * export default defineConfig({
+ *   plugins: [airx()]
+ * })
+ */
 export function VitePluginAirx() {
   return {
     name: 'airx',
